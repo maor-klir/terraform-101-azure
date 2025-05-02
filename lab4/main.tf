bf85383d1ab9dd4b4d28de703fed0dcf31f8a49b
maor-klir <maor.klir@gmail.com>
2025-05-02 13:24:33 +0200

    create an Azure Key Vault, Role Assignment, and enable RBAC on Key Vault

---
+++ lab4/main.tf
@@ -0,0 +1,28 @@
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-terraform-101-${var.application_name}-${var.environment_name}"
+  location = var.primary_location
+}
+
+resource "random_string" "keyvault-suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+  name                      = "kv-${var.application_name}-${var.environment_name}-${random_string.keyvault-suffix.result}"
+  location                  = azurerm_resource_group.main.location
+  resource_group_name       = azurerm_resource_group.main.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
+  enable_rbac_authorization = true
+}
+
+resource "azurerm_role_assignment" "terraform_user" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
