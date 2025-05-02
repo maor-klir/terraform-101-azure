501d774b334295c147b19fb7f3e07244cb69eb25
maor-klir <maor.klir@gmail.com>
2025-05-02 20:23:18 +0200

    Lab 5: provision vnet + subnets

---
+++ lab5/main.tf
@@ -0,0 +1,38 @@
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-terraform-101-${var.application_name}-${var.environment_name}"
+  location = var.primary_location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${var.application_name}-${var.environment_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.100.0.0/20"]
+}
+
+resource "azurerm_subnet" "alpha" {
+  name                 = "snet-alpha"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.100.0.0/24"]
+}
+resource "azurerm_subnet" "beta" {
+  name                 = "snet-beta"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.100.1.0/24"]
+}
+resource "azurerm_subnet" "gamma" {
+  name                 = "snet-gamma"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.100.2.0/24"]
+}
+resource "azurerm_subnet" "delta" {
+  name                 = "snet-delta"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.100.3.0/24"]
+}
+
